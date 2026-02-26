954823475bb8c7760552b95d78840c3ff090a4b0
Syed Wasi Ali <syedwasiali10@gmail.com>
2026-02-26 13:17:17 +0530

    26th Feb 1PM

--- src/components/gallery/gallery-one.tsx
+++ src/components/gallery/gallery-one.tsx
@@ -1,29 +1,30 @@
-import React, { CSSProperties } from 'react';
-import Image from 'next/image';
-import Marquee from 'react-fast-marquee';
+import React, { CSSProperties } from "react";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+
 // images
-import shape_1 from '@/assets/img/home-03/gallery/gal-shape-1.png';
-import shape_d_1 from '@/assets/img/home-03/gallery/gal-shape-dark-1.png';
-import shape_2 from '@/assets/img/home-03/gallery/gal-shape-2.png';
-import shape_d_2 from '@/assets/img/home-03/gallery/gal-shape-dark-2.png';
+import shape_1 from "@/assets/img/home-03/gallery/gal-shape-1.png";
+import shape_d_1 from "@/assets/img/home-03/gallery/gal-shape-dark-1.png";
+import shape_2 from "@/assets/img/home-03/gallery/gal-shape-2.png";
+import shape_d_2 from "@/assets/img/home-03/gallery/gal-shape-dark-2.png";
 
-// New gallery images from 6-products-gallery
+// gallery images from 6-products-gallery
 const gallery_images = [
-  '/assets/img/6-products-gallery/1.webp',
-  '/assets/img/6-products-gallery/2.webp',
-  '/assets/img/6-products-gallery/3.webp',
-  '/assets/img/6-products-gallery/4.webp',
-  '/assets/img/6-products-gallery/5.webp',
-  '/assets/img/6-products-gallery/6.webp',
-  '/assets/img/6-products-gallery/1.webp',
-  '/assets/img/6-products-gallery/2.webp',
-  '/assets/img/6-products-gallery/3.webp',
-  '/assets/img/6-products-gallery/4.webp',
-  '/assets/img/6-products-gallery/5.webp',
-  '/assets/img/6-products-gallery/6.webp',
-]
+  "/assets/img/6-products-gallery/1.webp",
+  "/assets/img/6-products-gallery/2.webp",
+  "/assets/img/6-products-gallery/3.webp",
+  "/assets/img/6-products-gallery/4.webp",
+  "/assets/img/6-products-gallery/5.webp",
+  "/assets/img/6-products-gallery/6.webp",
+  "/assets/img/6-products-gallery/1.webp",
+  "/assets/img/6-products-gallery/2.webp",
+  "/assets/img/6-products-gallery/3.webp",
+  "/assets/img/6-products-gallery/4.webp",
+  "/assets/img/6-products-gallery/5.webp",
+  "/assets/img/6-products-gallery/6.webp",
+];
 
-const imgStyle:CSSProperties = {height: "auto"};
+const imgStyle: CSSProperties = { height: "auto" };
 
 export default function GalleryOne() {
   return (
@@ -41,20 +42,18 @@ export default function GalleryOne() {
           <div className="col-xl-12">
             <div className="tp-gallery-slider-wrap">
               <div className="swiper-container tp-gallery-slider-active">
-                <Marquee className="tp-gallery-titming" speed={100} direction='left'>
-
+                <Marquee
+                  className="tp-gallery-titming"
+                  speed={100}
+                  direction="left"
+                >
                   {gallery_images.map((g, i) => (
-
                     <div key={i}>
                       <div className="tp-gallery-item mr-30">
-                        <img 
-                          src={g} 
-                          alt="gallery-img" 
-                        />
+                        <img src={g} alt="gallery-img" />
                       </div>
                     </div>
                   ))}
-
                 </Marquee>
               </div>
             </div>
@@ -62,5 +61,6 @@ export default function GalleryOne() {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
